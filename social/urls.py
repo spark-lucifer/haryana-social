5949499ad8c49e2b8585d10649a40779e6273beb
Piyush chaudhary <piyushchy05@gmail.com>
2024-01-23 10:49:03 +0530

    deploy

---
+++ social/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from .views import PostListView, PostDetailView,PostEditView,PostDeleteView,CommentDeleteView,ProfileView,AddLike,AddDislike,Code
+urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/code',Code.as_view(), name='code'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/like', AddLike.as_view(), name='like'),
+    path('post/<int:pk>/dislike', AddDislike.as_view(), name='dislike'),
+    path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+]
---
+++ zoom/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
