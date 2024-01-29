17daae0ab04b0cf80fa42480494a07dcadab2d04
Piyush chaudhary <piyushchy05@gmail.com>
2024-01-29 21:18:57 +0530

    sign up fixed

--- web-scraping-2/social/models.py
+++ web-scraping-2/social/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class Post(models.Model):
     body = models.TextField()
@@ -27,3 +29,12 @@ class Game(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+@receiver(post_save, sender=User)
+def create_profile(sender,instance,created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender,instance,**kwargs):
+    instance.profile.save()
