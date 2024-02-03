1737898ac4f289ec483f25fe8d2e06b65ba099d6
Piyush chaudhary <piyushchy05@gmail.com>
2024-02-04 00:22:49 +0530

    add default profile image

--- web-scraping-2/social/models.py
+++ web-scraping-2/social/models.py
@@ -24,7 +24,7 @@ class UserProfile(models.Model):
     bio = models.TextField(max_length=500,blank=True,null=True)
     birth_date = models.DateField(null=True,blank=True)
     location = models.CharField(max_length=100,blank=True,null=True)
-    profile_pic = models.ImageField(upload_to='uploads/profile_pictures',default='uploads/profile_pictures/default.png', blank=True)
+    profile_pic = models.ImageField(upload_to='uploads/profile_pictures',default='uploads/profile_pictures/default.jpg', blank=True)
 
 class Game(models.Model):
    
